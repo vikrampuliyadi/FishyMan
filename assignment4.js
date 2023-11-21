ca3e8c36e618cd5b85be5cef1ece7cbc9f5f6eb4
Ryanung3873 <ryanung3873@g.ucla.edu>
2023-11-21 11:06:08 -0800

    sand sphere and water cylinder

--- assignment4.js
+++ assignment4.js
@@ -1,112 +1,88 @@
-import {defs, tiny} from './examples/common.js';
+import { defs, tiny } from './examples/common.js';
 
 const {
-    Vector, Vector3, vec, vec3, vec4, color, hex_color, Shader, Matrix, Mat4, Light, Shape, Material, Scene, Texture,
+  Vector, vec3, vec4, color, hex_color, Matrix, Mat4, Light, Shape, Material, Scene, Texture,
 } = tiny;
 
-const {Cube, Axis_Arrows, Textured_Phong} = defs
+const { Cube, Axis_Arrows, Textured_Phong, Subdivision_Sphere, Textured_Phong_Shader } = defs;
 
 export class Assignment4 extends Scene {
-    /**
-     *  **Base_scene** is a Scene that can be added to any display canvas.
-     *  Setup the shapes, materials, camera, and lighting here.
-     */
-    constructor() {
-        // constructor(): Scenes begin by populating initial values like the Shapes and Materials they'll need.
-        super();
+  constructor() {
+    super();
 
-        // TODO:  Create two cubes, including one with the default texture coordinates (from 0 to 1), and one with the modified
-        //        texture coordinates as required for cube #2.  You can either do this by modifying the cube code or by modifying
-        //        a cube instance's texture_coords after it is already created.
-        this.shapes = {
-            box_1: new Cube(),
-            box_2: new Cube(),
-            axis: new Axis_Arrows()
-        }
-        console.log(this.shapes.box_1.arrays.texture_coord)
+    this.shapes = {
+      box_1: new Cube(),
+      box_2: new Cube(),
+      axis: new Axis_Arrows(),
+      sphere: new Subdivision_Sphere(4),
+      sand: new defs.Capped_Cylinder(50, 50, [[0, 2], [0, 1]]),
+    };
 
+    const textured = new Textured_Phong(1); // Use Textured_Phong shader
 
-        // TODO:  Create the materials required to texture both cubes with the correct images and settings.
-        //        Make each Material from the correct shader.  Phong_Shader will work initially, but when
-        //        you get to requirements 6 and 7 you will need different ones.
-        this.materials = {
-            phong: new Material(new Textured_Phong(), {
-                color: hex_color("#ffffff"),
-            }),
-            texture: new Material(new Textured_Phong(), {
-                color: hex_color("#ffffff"),
-                ambient: 0.5, diffusivity: 0.1, specularity: 0.1,
-                texture: new Texture("assets/stars.png")
-            }),
-        }
+    this.materials = {
+      water: new Material(textured, { ambient: 0.5, texture: new Texture("assets/water.jpeg") }),
+      sand: new Material(textured, {
+        ambient: 0.3, diffusivity: 0.9, color: hex_color("#ffaf40"), smoothness: 64,
+        texture: new Texture("assets/sand3.png"),
+        light_depth_texture: null,
+      }),
+      sky: new Material(textured, {
+        ambient: 0.5,
+        texture: new Texture("assets/sky_three.jpeg"),
+      }),
+    };
 
-        this.initial_camera_location = Mat4.look_at(vec3(0, 10, 20), vec3(0, 0, 0), vec3(0, 1, 0));
+    this.light_view_target = vec4(0, 0, 0, 1); // Declare light_view_target
+
+    this.initial_camera_location = Mat4.look_at(vec3(0, -20, 0), vec3(0, 0, 0), vec3(0, 0, 1));
+  }
+
+  make_control_panel() {
+    // TODO: Implement requirement #5 using a key_triggered_button that responds to the 'c' key.
+  }
+
+  display(context, program_state) {
+    if (!context.scratchpad.controls) {
+      this.children.push(context.scratchpad.controls = new defs.Movement_Controls());
+      program_state.set_camera(Mat4.translation(-3.13, -5.05, -25.24));
+      //facing east down north
     }
 
-    make_control_panel() {
-        // TODO:  Implement requirement #5 using a key_triggered_button that responds to the 'c' key.
-    }
+    // Set the background color (light blue)
+    program_state.projection_transform = Mat4.perspective(
+      Math.PI / 4, context.width / context.height, 1, 100
+    );
 
-    display(context, program_state) {
-        if (!context.scratchpad.controls) {
-            this.children.push(context.scratchpad.controls = new defs.Movement_Controls());
-            // Define the global camera and projection matrices, which are stored in program_state.
-            program_state.set_camera(Mat4.translation(0, 0, -8));
-        }
+    // Set up directional light
+    const light_position = vec4(0, 10, 0, 0); // Directional light from above
+    program_state.lights = [new Light(light_position, color(1, 1, 1, 1), 1000)];
 
-        program_state.projection_transform = Mat4.perspective(
-            Math.PI / 4, context.width / context.height, 1, 100);
+    let t = program_state.animation_time / 1000, dt = program_state.animation_delta_time / 1000;
+    let model_transform = Mat4.identity();
 
-        const light_position = vec4(10, 10, 10, 1);
-        program_state.lights = [new Light(light_position, color(1, 1, 1, 1), 1000)];
+    // // Draw water background
+    // let background_transform = model_transform.times(Mat4.scale(60, 60, 60))
+    //   .times(Mat4.rotation(0, 0, 1, 0))
+    //   .times(Mat4.rotation(Math.PI / 1.8, 1, 0, 0))
+    //   .times(Mat4.rotation(t / 40000, 0, 1, 0));
+    //
+    // this.shapes.sphere.draw(context, program_state, background_transform, this.materials.water);
 
-        let t = program_state.animation_time / 1000, dt = program_state.animation_delta_time / 1000;
-        let model_transform = Mat4.identity();
+    let ocean_transform = model_transform.times(Mat4.rotation(1, 1, 1, 0))
+      .times(Mat4.rotation(Math.PI / 2, 1, 0, 0))
+      .times(Mat4.translation(0, 0, 2))
+      .times(Mat4.scale(100, 100, 1));
+    //
+    this.shapes.sphere.draw(context, program_state, ocean_transform, this.materials.water);
 
-        // TODO:  Draw the required boxes. Also update their stored matrices.
-        // You can remove the folloeing line.
-        this.shapes.axis.draw(context, program_state, model_transform, this.materials.phong.override({color: hex_color("#ffff00")}));
-    }
+  // Draw sand sphere
+    let sand_transform = model_transform.times(Mat4.rotation(1, 1, 1, 0))
+      .times(Mat4.rotation(Math.PI / 2, 1, 0, 0))
+      .times(Mat4.translation(2, 2, 2))
+      .times(Mat4.scale(10, 10, 3));
+
+
+    this.shapes.sphere.draw(context, program_state, sand_transform, this.materials.sand);
+  }
 }
-
-
-class Texture_Scroll_X extends Textured_Phong {
-    // TODO:  Modify the shader below (right now it's just the same fragment shader as Textured_Phong) for requirement #6.
-    fragment_glsl_code() {
-        return this.shared_glsl_code() + `
-            varying vec2 f_tex_coord;
-            uniform sampler2D texture;
-            uniform float animation_time;
-            
-            void main(){
-                // Sample the texture image in the correct place:
-                vec4 tex_color = texture2D( texture, f_tex_coord);
-                if( tex_color.w < .01 ) discard;
-                                                                         // Compute an initial (ambient) color:
-                gl_FragColor = vec4( ( tex_color.xyz + shape_color.xyz ) * ambient, shape_color.w * tex_color.w ); 
-                                                                         // Compute the final color with contributions from lights:
-                gl_FragColor.xyz += phong_model_lights( normalize( N ), vertex_worldspace );
-        } `;
-    }
-}
-
-
-class Texture_Rotate extends Textured_Phong {
-    // TODO:  Modify the shader below (right now it's just the same fragment shader as Textured_Phong) for requirement #7.
-    fragment_glsl_code() {
-        return this.shared_glsl_code() + `
-            varying vec2 f_tex_coord;
-            uniform sampler2D texture;
-            uniform float animation_time;
-            void main(){
-                // Sample the texture image in the correct place:
-                vec4 tex_color = texture2D( texture, f_tex_coord );
-                if( tex_color.w < .01 ) discard;
-                                                                         // Compute an initial (ambient) color:
-                gl_FragColor = vec4( ( tex_color.xyz + shape_color.xyz ) * ambient, shape_color.w * tex_color.w ); 
-                                                                         // Compute the final color with contributions from lights:
-                gl_FragColor.xyz += phong_model_lights( normalize( N ), vertex_worldspace );
-        } `;
-    }
-}
-
